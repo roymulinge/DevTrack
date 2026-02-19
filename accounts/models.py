10f855797dbf25a315626c564aa228dd90151484
Roy <mutuaroymulinge@gmail.com>
2026-02-20 00:39:41 +0300

    add create_superuser method

--- accounts/models.py
+++ accounts/models.py
@@ -10,3 +10,9 @@ class UserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
+    
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+    
