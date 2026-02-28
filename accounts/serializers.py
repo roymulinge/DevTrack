1dffa040e9fc9ebee97103c7bbc5b467e166ff40
Roy <mutuaroymulinge@gmail.com>
2026-02-28 07:49:56 +0300

    add permission file and serializer in dev_app

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -6,7 +6,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
 
-    class meta:
+    class Meta:
         model = User
         fields = ('email', 'full_name', 'password', 'password2')
 
--- dev_app/models.py
+++ dev_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Project(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='projects'
+    )
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
