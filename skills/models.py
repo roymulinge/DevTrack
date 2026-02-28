599f575dc8cfa2a46e12f925ed120c32b574e3b5
Roy <mutuaroymulinge@gmail.com>
2026-02-28 15:42:24 +0300

    feat(skills):add meta model with verbose name and indexes

--- skills/models.py
+++ skills/models.py
@@ -7,6 +7,13 @@ class Skill(OwnedModel):
     category = models.CharField(max_length=100)
     depth_level = models.IntegerField()
     last_practiced = models.DateField()
-
+    
+    class Meta:
+        verbose_name = "Skill"
+        verbose_name_plural = "Skills"
+        indexes = [
+            models.Index(fields=["owner"]),
+            models.Index(fields=["last_practiced"]),
+        ]
     def __str__(self):
         return self.name
