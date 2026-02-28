f4c9fd988afaaee49f41d939be07e63810de078e
Roy <mutuaroymulinge@gmail.com>
2026-02-28 16:29:09 +0300

    feat(skill):  add serializer, viewset, and ownership protection

---
+++ skills/urls.py
@@ -0,0 +1,7 @@
+from rest_framework.routers import DefaultRouter
+from .views import SkillViewSet
+
+router = DefaultRouter()
+router.register(r"skills", SkillViewSet)
+
+urlpatterns = router.urls
--- skills/views.py
+++ skills/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from core.mixins import OwnerQuerySetMixin
+from core.permissions import IsOwner
+from .models import Skill
+from .serializer import SkillSerializer
 
-# Create your views here.
+
+class SkillViewSet(OwnerQuerySetMixin, ModelViewSet):
+    serializer_class = SkillSerializer
+    permission_classes = [IsAuthenticated, IsOwner]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
