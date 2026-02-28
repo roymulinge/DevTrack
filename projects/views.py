fcc8962ac18c8089c8c9182175919b7d323bedfd
Roy <mutuaroymulinge@gmail.com>
2026-02-28 15:32:31 +0300

    feat(projects):Add serialzer and  implement onwership handling

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from core.mixins import OwnerQuerySetMixin
+from core.permissions import IsOwner
+from .models import Project
+from .serializer import ProjectSerializer
+from projects import serializer
 
-# Create your views here.
+class ProjectViewSet(OwnerQuerySetMixin, ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated, IsOwner]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
