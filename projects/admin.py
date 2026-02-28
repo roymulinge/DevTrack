d4002149339429271947f10497c389421384004b
Roy <mutuaroymulinge@gmail.com>
2026-02-28 15:14:09 +0300

    feat(projects):register project and assignment class in admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Project, Assignment
 
-# Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("name", "owner", "priority", "status", "created_at")
+    search_fields = ("name",)
+    list_filter = ("status", "priority")
+
+
+@admin.register(Assignment)
+class AssignmentAdmin(admin.ModelAdmin):
+    list_display = ("title", "project", "owner", "deadline", "completed")
+    list_filter = ("completed", "deadline")
