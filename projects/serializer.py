308df2eafc4bd6cf411703c21025c930fce1668b
Roy <mutuaroymulinge@gmail.com>
2026-03-11 09:22:54 +0300

    ref(projects): add assignmentserializer

--- projects/serializer.py
+++ projects/serializer.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Project
+from .models import Project, Assignment
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
+        fields = "__all__"
+        read_only_fields = ["owner"]
+
+class AssignmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Assignment
         fields = "__all__"
         read_only_fields = ["owner"]
