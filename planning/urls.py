4279dd70f09bd7d4ed798fc28260d9f9e9383345
Roy <mutuaroymulinge@gmail.com>
2026-03-11 09:12:44 +0300

    reafactor(planning):  app planningweekly url path

--- planning/urls.py
+++ planning/urls.py
@@ -1,7 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from .views import WeeklyPriorityViewSet
-
+from .views import WeeklyPriorityViewSet, WeeklySummaryView
+from django.urls import path
 router = DefaultRouter()
 router.register(r"weekly-priorities", WeeklyPriorityViewSet, basename="weeklypriority")
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path("planning/weekly-summary/", WeeklySummaryView.as_view(), name="weekly-summary"),
+]
