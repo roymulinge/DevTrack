d1146f75d4dc5e1a37797e63cd4e16e0b3ca0bc3
Roy <mutuaroymulinge@gmail.com>
2026-03-14 20:28:17 +0300

    feat(projects): register AssignmentViewSet in projects urls

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,7 @@
 from rest_framework.routers import DefaultRouter
-from .views import ProjectViewSet
+from .views import ProjectViewSet, AssignmentViewSet
 
 router = DefaultRouter()
 router.register(r"projects", ProjectViewSet, basename="project")
-
+router.register(r"assignments", AssignmentViewSet, basename="assignment")
 urlpatterns = router.urls
