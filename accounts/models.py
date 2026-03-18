1598ca4e42129e69edf52e262de13142756446f8
Roy <mutuaroymulinge@gmail.com>
2026-03-18 11:52:06 +0300

    feat(accounts): add emails.py file, Fix(accounts):import uuid in models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+import uuid
 # Create your models here.
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
