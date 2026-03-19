eb7042ff6824915d850bfd180edb2a5a7dc7b3a6
Roy <mutuaroymulinge@gmail.com>
2026-03-19 07:45:50 +0300

    feat(dev_track):add celery.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView,VerifyEmailView, ResendVerificationView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('verify-email/<uuid:token>/', VerifyEmailView.as_view(),  name='verify_email'),
+    path('resend-verification/',  ResendVerificationView.as_view(),name='resend_verification'),
 ]
+
