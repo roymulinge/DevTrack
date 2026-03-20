ac0538c441678ad21285b281f831b8f23d41a9e1
Roy <mutuaroymulinge@gmail.com>
2026-03-20 14:34:36 +0300

    fix(skills): update skills serializer to inculde notes

--- skills/serializer.py
+++ skills/serializer.py
@@ -5,5 +5,5 @@ from .models import Skill
 class SkillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Skill
-        fields = "__all__"
-        read_only_fields = ["owner"]
+        fields = ["id", "name", "category", "depth_level", "last_practiced", "notes", "owner", "created_at"]
+        read_only_fields = ["owner", "created_at"]
