f0d9a696984f66a9132ed5ea9c76a36ecff81ad3
Roy <mutuaroymulinge@gmail.com>
2026-03-22 17:00:39 +0300

    feat: add profile, update name and delete account endpoints

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path
-from .views import ChangePasswordView, GoogleLoginView, RegisterView,VerifyEmailView, ResendVerificationView
+from .views import (
+    RegisterView, VerifyEmailView, ResendVerificationView,
+    GoogleLoginView, ChangePasswordView, ProfileView, DeleteAccountView
+)
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('verify-email/<uuid:token>/', VerifyEmailView.as_view(),  name='verify_email'),
-    path('resend-verification/',  ResendVerificationView.as_view(),name='resend_verification'),
-    path('google/', GoogleLoginView.as_view(), name='google_login'),
-     path('change-password/', ChangePasswordView.as_view(), name='change_password'),
+    path('register/',                  RegisterView.as_view(),            name='register'),
+    path('login/',                     TokenObtainPairView.as_view(),     name='token_obtain_pair'),
+    path('token/refresh/',             TokenRefreshView.as_view(),        name='token_refresh'),
+    path('verify-email/<uuid:token>/', VerifyEmailView.as_view(),        name='verify_email'),
+    path('resend-verification/',       ResendVerificationView.as_view(),  name='resend_verification'),
+    path('google/',                    GoogleLoginView.as_view(),         name='google_login'),
+    path('change-password/',           ChangePasswordView.as_view(),      name='change_password'),
+    path('me/',                        ProfileView.as_view(),             name='profile'),
+    path('delete/',                    DeleteAccountView.as_view(),       name='delete_account'),
 ]
 
