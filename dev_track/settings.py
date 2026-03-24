3e548f8c8fb20dc5e293bdfc5021acec9aec1601
Roy <mutuaroymulinge@gmail.com>
2026-03-24 12:56:19 +0300

    feat(settings): add BLACKLIST_AFTER_ROTATION in SIMLPLE_JWT to prevent old refresh token from reuse

--- dev_track/settings.py
+++ dev_track/settings.py
@@ -151,6 +151,7 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME":  timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS":  True,
+    "BLACKLIST_AFTER_ROTATION": True,
 }
 
 LANGUAGE_CODE = 'en-us'
