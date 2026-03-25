b221cf158deeb1795121453d74731b3565f72e03
Roy <mutuaroymulinge@gmail.com>
2026-03-25 11:41:56 +0300

    feat(accounts):  add ForgotPasswordSerializer and ResetPasswordSerializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from .models import User
 from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils.http import urlsafe_base64_decode
+from django.utils.encoding import force_str
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
@@ -36,4 +39,27 @@ class ChangePasswordSerializer(serializers.Serializer):
     def validate(self, attrs):
         if attrs['new_password'] != attrs['newpassword2']:
             raise serializers.ValidationError({"new_password": "New password do not match."})
+        return attrs
+
+class ForgotPasswordSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+
+class ResetPasswordSerializer(serializers.Serializer):
+    uid = serializers.CharField()
+    token = serializers.CharField()
+    password = serializers.CharField(required=True, validators=[validate_password])
+
+    def validate(self, attrs):
+        try:
+            user_id = force_str(urlsafe_base64_decode(attrs['uid']))
+            user = User.objects.get(pk=user_id)
+        except Exception:
+            raise serializers.ValidationError({"error": "Invalid or expired token"})
+
+        token_generator = PasswordResetTokenGenerator()
+        if not token_generator.check_token(user, attrs['token']):
+            raise serializers.ValidationError({"error": "Invalid or expired token"})
+
+        attrs['user'] = user
         return attrs
