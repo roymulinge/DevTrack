2ea23f7b85074ba8fdbdbf3d5ef01c542c69cf28
Roy <mutuaroymulinge@gmail.com>
2026-04-07 20:24:54 +0300

    feat(planning): add next-action in plannig urls

--- planning/urls.py
+++ planning/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from .views import WeeklyPriorityViewSet, WeeklySummaryView, DailyFocusView
+from .views import NextActionView, WeeklyPriorityViewSet, WeeklySummaryView, DailyFocusView
 from django.urls import path
 router = DefaultRouter()
 router.register(r"weekly-priorities", WeeklyPriorityViewSet, basename="weeklypriority")
@@ -7,4 +7,5 @@ router.register(r"weekly-priorities", WeeklyPriorityViewSet, basename="weeklypri
 urlpatterns = router.urls + [
     path("planning/weekly-summary/", WeeklySummaryView.as_view(), name="weekly-summary"),
      path("planning/daily-focus/", DailyFocusView.as_view(), name="daily-focus"),
+     path("planning/next-action/", NextActionView.as_view(), name="next-action"),
 ]
