9500b92a5c465150da54e9f17e9a8f7d395d8903
Roy <mutuaroymulinge@gmail.com>
2026-05-25 13:05:50 +0300

    feat(dev_track): add notification url to projects root url pattern

--- dev_track/urls.py
+++ dev_track/urls.py
@@ -32,4 +32,5 @@ urlpatterns = [
     path("api/", include("ideas.urls")),
     path("api/", include("planning.urls")),
     path("api/core/", include("core.urls")),
+    path("api/notifications/", include("notifications.urls")),
 ]
